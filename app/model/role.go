e70951ecc692fe99fc9cb8893aac6968a8e3ade2
hello-express <liuhaobin486@gmail.com>
2022-10-13 11:46:49 +0800

    model

---
+++ app/model/role.go
@@ -0,0 +1,32 @@
+package model
+
+import (
+	"github.com/jinzhu/gorm"
+	"time"
+)
+
+type Role struct {
+	Id        uint       `json:"id"`
+	RoleName  string     `json:"role_name"`
+	Detail    string     `json:"detail"`
+	CreateId  int        `json:"create_id"`
+	UpdateId  int        `json:"update_id"`
+	RoleAuths []RoleAuth `json:"-" gorm:"foreignkey:RoleID"`
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt time.Time  `json:"updated_at"`
+}
+
+func (r *Role) BeforeCreate(scope *gorm.Scope) {
+	scope.SetColumn("Status", 1)
+}
+
+func (r *Role) BeforeUpdate(scope *gorm.Scope) {
+
+}
+
+type RoleEditShow struct {
+	ID       int
+	RoleName string
+	Status   int
+	Checked  int
+}
