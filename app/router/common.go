66c4f550046f7e613666fa5ee08f131050c5741a
hello-express <liuhaobin486@gmail.com>
2022-10-13 22:20:47 +0800

    fix

--- app/router/common.go
+++ app/router/common.go
@@ -2,7 +2,16 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	controller "pear-admin-golang/app/controller"
+	"pear-admin-golang/app/middleware"
 )
 
 func CommonRouter(common *gin.Engine) {
+	common.GET("/", middleware.CheckDefaultPage)
+	common.GET("login", middleware.CheckLoginPage, controller.Login)
+	common.POST("login", controller.LoginHandler)
+	common.GET("logout", controller.Logout)
+	common.GET("not_found", controller.NotFound)
+	common.GET("captcha", controller.GetCaptcha)
+	common.POST("captcha_verify", controller.CaptchaVerify)
 }
